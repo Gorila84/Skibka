5aef0639a73b1cc1e63895bfd42a3e2903c2af7b
KF-BERG <79504951+KF-BERG@users.noreply.github.com>
2023-10-23 11:28:34 +0200

    test

---
+++ gastrosalon_net4_8/Program.cs
@@ -0,0 +1,29 @@
+﻿using gastrosalon_net4_8.Helpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gastrosalon_net4_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            //string xml = File.ReadAllText("stalgast_catalog.xml");
+            ////Deserialize the XML back into an object
+            //stalgast_catalog catalog = XmlConverter<stalgast_catalog>.ToObject(xml);
+
+
+            string xml = File.ReadAllText("RM GASTRO XML products.xml");
+            //Deserialize the XML back into an object
+            products catalog = XmlConverter<products>.ToObject(xml);
+
+       }
+    }
+}
